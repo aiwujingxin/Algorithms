a87ce3ea05222ee6d39f8a01b4798ae59e889459
Jingxin Wu <jingxin.wu@shopee.com>
2023-08-16 23:22:32 +0800

    refactor: git

---
+++ java/src/basic/structure/tree/build/PostAndInOrder.java
@@ -0,0 +1,16 @@
+package basic.structure.tree.build;
+
+import basic.structure.tree.BuildTree;
+import common.TreeNode;
+
+/**
+ * @author wujingxinit@outlook.com
+ * @date 2022/9/7 13:04
+ * {@link leetcode.problems.LeetCode106}
+ */
+public class PostAndInOrder implements BuildTree {
+    @Override
+    public TreeNode buildTree(int[] inorder, int[] post) {
+        return null;
+    }
+}
