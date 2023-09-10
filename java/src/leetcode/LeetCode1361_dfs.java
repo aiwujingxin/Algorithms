a2f737105408d1a837caecae1a1818ab415ee0e9
jingxin.wu <jingxin.wu@shopee.com>
2023-09-10 14:01:55 +0800

    feat: graph mind

--- java/src/leetcode/LeetCode1361_dfs.java
+++ java/src/leetcode/LeetCode1361_dfs.java
@@ -1,87 +1,52 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * @author aiwujingxin@gmail.com
- * @date 2022/6/25 23:36
+ * @date 2022/6/25 23:58
  */
 public class LeetCode1361_dfs {
 
-    //https://leetcode.com/problems/validate-binary-tree-nodes/discuss/1814452/Java-Simple-Solution-Using-DFS
-
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-
-        // build graph
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
-        int[] indegree = new int[n];
-        for (int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
+        Integer root = null;
+        boolean[] visited = new boolean[leftChild.length];
+        for (int i = 0; i < leftChild.length; i++) {
             if (leftChild[i] != -1) {
-                graph.get(i).add(leftChild[i]);
-                indegree[leftChild[i]]++;
+                visited[leftChild[i]] = true;
             }
             if (rightChild[i] != -1) {
-                graph.get(i).add(rightChild[i]);
-                indegree[rightChild[i]]++;
+                visited[rightChild[i]] = true;
             }
-            if (graph.get(i).size() > 2) {
+        }
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                if (root != null) {
+                    return false;
+                }
+                root = i;
+            }
+        }
+        visited = new boolean[leftChild.length];
+        if (root == null) {
+            return false;
+        }
+        boolean ans = dfs(root, leftChild, rightChild, visited);
+        for (boolean ele : visited) {
+            if (!ele) {
                 return false;
             }
         }
-
-        int components = 0;
-
-        // 判断入度
-        for (int i : indegree) {
-            // root 节点
-            if (i == 0) {
-                components++;
-                if (components > 1) {
-                    return false;
-                }
-
-            }
-            if (i > 1) {
-                return false;
-            }
-        }
-
-        int[] visited = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                if (isCyclic(graph, i, visited)) {
-                    return false;
-                }
-
-            }
-        }
-        return true;
+        return ans;
     }
 
-    // 判断有无环 todo 1 ？ 2 ？
-    public boolean isCyclic(HashMap<Integer, List<Integer>> graph, int src, int[] visited) {
-        if (visited[src] == 2) {
+    public boolean dfs(int root, int[] left, int[] right, boolean[] visited) {
+        if (root == -1) {
             return true;
         }
-
-        visited[src] = 2;
-
-        for (int i : graph.get(src)) {
-            if (visited[i] != 1) {
-                if (isCyclic(graph, i, visited)) {
-                    return true;
-                }
-            }
+        if (visited[root]) {
+            return false;
         }
-
-        visited[src] = 1;
-
-        return false;
+        visited[root] = true;
+        return dfs(left[root], left, right, visited) && dfs(right[root], left, right, visited);
     }
 }
--- java/src/leetcode/LeetCode1361_topo.java
+++ java/src/leetcode/LeetCode1361_topo.java
@@ -1,64 +1,64 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 
 /**
  * @author aiwujingxin@gmail.com
- * @date 2022/6/25 23:36
+ * @date 2022/6/25 23:52
  */
 public class LeetCode1361_topo {
+
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        List<Integer>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
-        int edges = 0;
-        int[] degree = new int[n];
+        // handle find 1 root
+        boolean[] isRoot = new boolean[n];
+        Arrays.fill(isRoot, true);
         for (int i = 0; i < n; i++) {
             if (leftChild[i] != -1) {
-                graph[i].add(leftChild[i]);
-                degree[leftChild[i]]++;
-                edges++;
+                isRoot[leftChild[i]] = false;
             }
             if (rightChild[i] != -1) {
-                graph[i].add(rightChild[i]);
-                degree[rightChild[i]]++;
-                edges++;
+                isRoot[rightChild[i]] = false;
             }
-            if (i == leftChild[i] || i == rightChild[i] || (leftChild[i] != -1 && leftChild[i] == rightChild[i]))
-                return false;
         }
-        if (edges != n - 1) return false;
 
-        int count = 0;
-
-        Queue<Integer> queue = new LinkedList<>();
+        int rootCount = 0;
+        int root = -1;
         for (int i = 0; i < n; i++) {
-            if (degree[i] == 0) {
-                queue.offer(i);
-            }
-            if (degree[i] > 1) {
-                return false;
+            if (isRoot[i]) {
+                rootCount++;
+                root = i;
             }
         }
+        if (rootCount != 1) {
+            return false;
+        }
 
-        if (queue.size() > 1) return false;
-
+        // bfs to find cycle
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(root);
 
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int curr = queue.poll();
-                count++;
-                for (int w : graph[curr]) {
-                    if (--degree[w] == 0) {
-                        queue.offer(w);
-                    }
-                }
+            int cur = queue.poll();
+            count++;
+            if (visited[cur]) {
+                return false;
+            }
+            visited[cur] = true;
+
+            if (leftChild[cur] != -1) {
+                queue.add(leftChild[cur]);
+            }
+
+            if (rightChild[cur] != -1) {
+                queue.add(rightChild[cur]);
             }
         }
-        return count == n;
 
+        return count == n;
     }
 }
