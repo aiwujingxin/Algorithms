1464b1ebffe5e9882911619cd2de0a47e1f4ddba
Jingxin Wu <jingxin.wu@shopee.com>
2023-09-28 17:04:44 +0800

    lcr

--- java/src/basic/algorithm/dp/string/Palindrome.java
+++ java/src/basic/algorithm/dp/string/Palindrome.java
@@ -3,6 +3,7 @@ package basic.algorithm.dp.string;
 /**
  * @author wujingxinit@outlook.com
  * @date 2023/8/21 23:01
+ * @see basic.datastructure.string.palindrome.Palindrome
  */
 public interface Palindrome {
 }
