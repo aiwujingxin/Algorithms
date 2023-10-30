5e43bfe7ed83e8432eab22a337b3ef005b37c029
Jingxin Wu <jingxin.wu@shopee.com>
2023-10-31 00:28:21 +0800

    100-200

--- java/src/leetcode/problems/LeetCode135.java
+++ java/src/leetcode/problems/LeetCode135.java
@@ -1,28 +1,38 @@
 package leetcode.problems;
 
+/**
+ * @author wujingxinit@outlook.com
+ * @date 2023/10/30 23:56
+ */
 public class LeetCode135 {
 
     public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        }
         int n = ratings.length;
         int[] left = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
-            } else {
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i - 1] >= ratings[i]) {
                 left[i] = 1;
-            }
-        }
-
-        int right = 0, ret = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
-                right++;
             } else {
-                right = 1;
+                left[i] = left[i - 1] + 1;
             }
-
-            ret += Math.max(left[i], right);
         }
-        return ret;
+        int[] right = new int[n];
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i + 1] >= ratings[i]) {
+                right[i] = 1;
+            } else {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res += Math.max(left[i], right[i]);
+        }
+        return res;
     }
 }
