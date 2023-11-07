af55c65197810dfd87f623e06c53b4878fe7b4ef
Jingxin Wu <jingxin.wu@shopee.com>
2023-11-07 23:01:06 +0800

    tidy

--- java/src/basic/algorithm/dp/knapsack/TargetSum.java
+++ java/src/basic/algorithm/dp/knapsack/TargetSum.java
@@ -1,11 +1,11 @@
 package basic.algorithm.dp.knapsack;
 
-import leetcode.problems.LeetCode494_dp_1d;
+import leetcode.problems.LeetCode494;
 
 /**
  * @author wujingxinit@outlook.com
  * @date 2022/9/13 13:40
- * @see LeetCode494_dp_1d  https://leetcode.cn/problems/target-sum/?favorite=2cktkvj
+ * @see LeetCode494  https://leetcode.cn/problems/target-sum/?favorite=2cktkvj
  */
 public interface TargetSum {
     int findTargetSumWays(int[] nums, int target);
--- java/src/basic/datastructure/string/palindrome/Palindrome.java
+++ java/src/basic/datastructure/string/palindrome/Palindrome.java
@@ -7,7 +7,7 @@ import leetcode.problems.*;
  * @date 2023/8/31 14:15
  * @see LeetCode5 5. 最长回文子串
  * @see LeetCode131 131. 分割回文串
- * @see LeetCode132_dp_2d 132. 分割回文串 II
+ * @see LeetCode132 132. 分割回文串 II
  * @see LeetCode516 516. 最长回文子序列
  * @see LeetCode647 647. 回文子串
  * @see LeetCode267 267. 回文排列 II
