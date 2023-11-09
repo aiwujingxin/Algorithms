14667bb4a4c5dfed29c466056d792fdd5a8f007d
jingxin.wu <jingxin.wu@shopee.com>
2023-11-09 16:42:03 +0800

    dp

---
+++ java/src/leetcode/problems/LeetCode1092.java
@@ -0,0 +1,36 @@
+package leetcode.problems;
+
+/**
+ * @author wujingxinit@outlook.com
+ * @date 2023/11/9 16:30
+ */
+public class LeetCode1092 {
+
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int m = str1.length(), n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (str1.charAt(i) == str2.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = m, j = n; i > 0 || j > 0; ) {
+            if (i == 0 || (j != 0 && dp[i][j] == dp[i][j - 1])) {
+                sb.append(str2.charAt(--j));
+                continue;
+            }
+            if (j == 0 || dp[i][j] == dp[i - 1][j]) {
+                sb.append(str1.charAt(--i));
+                continue;
+            }
+            sb.append(str1.charAt(--i));
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ java/src/leetcode/problems/LeetCode1312.java
@@ -0,0 +1,26 @@
+package leetcode.problems;
+
+/**
+ * @author wujingxinit@outlook.com
+ * @date 2023/11/9 16:26
+ */
+public class LeetCode1312 {
+
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return n - dp[0][n - 1];
+    }
+}
