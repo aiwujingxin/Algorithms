1bb74b75baf495e2025313b664c0c5ec39b3bc9f
jingxin.wu <jingxin.wu@shopee.com>
2023-12-01 18:52:04 +0800

    400+

--- java/src/leetcode/problems/LeetCode447.java
+++ java/src/leetcode/problems/LeetCode447.java
@@ -1,5 +1,8 @@
 package leetcode.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author wujingxinit@outlook.com
  * @date 2023/12/1 17:28
@@ -7,9 +10,18 @@ package leetcode.problems;
 public class LeetCode447 {
 
     public int numberOfBoomerangs(int[][] points) {
-        if (points == null || points.length == 0) {
-            return 0;
+        int ans = 0;
+        for (int[] p : points) {
+            Map<Integer, Integer> cnt = new HashMap<>();
+            for (int[] q : points) {
+                int dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                cnt.put(dis, cnt.getOrDefault(dis, 0) + 1);
+            }
+            for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+                int m = entry.getValue();
+                ans += m * (m - 1);
+            }
         }
-
+        return ans;
     }
 }
