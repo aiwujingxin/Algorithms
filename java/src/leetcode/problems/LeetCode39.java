b8841f59edec69ce3c2a301bd0e5a98f839c2e18
Jingxin Wu <jingxin.wu@shopee.com>
2023-12-04 11:16:51 +0800

    Backtrack

--- java/src/leetcode/problems/LeetCode39.java
+++ java/src/leetcode/problems/LeetCode39.java
@@ -17,7 +17,7 @@ public class LeetCode39 {
         return res;
     }
 
-    private void backtrack(int[] candidates, int index, int target, List<Integer> list, List<List<Integer>> res) {
+    private void backtrack(int[] candidates, int start, int target, List<Integer> list, List<List<Integer>> res) {
         if (target == 0) {
             res.add(new ArrayList<>(list));
             return;
@@ -25,7 +25,7 @@ public class LeetCode39 {
         if (target < 0) {
             return;
         }
-        for (int i = index; i < candidates.length; i++) {
+        for (int i = start; i < candidates.length; i++) {
             list.add(candidates[i]);
             backtrack(candidates, i, target - candidates[i], list, res);
             list.remove(list.size() - 1);
