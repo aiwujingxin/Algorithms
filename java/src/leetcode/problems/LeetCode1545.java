98dcec9c9a0b95ed6581cb4be229273f500a0045
Jingxin Wu <jingxin.wu@shopee.com>
2024-02-02 19:24:47 +0800

    减治

--- java/src/leetcode/problems/LeetCode1545.java
+++ java/src/leetcode/problems/LeetCode1545.java
@@ -7,30 +7,16 @@ package leetcode.problems;
  */
 public class LeetCode1545 {
     public char findKthBit(int n, int k) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("0");
-        while (sb.length() < k) {
-            int i = sb.length() - 1;
-            sb.append("1");
-            while (i >= 0) {
-                char c = sb.charAt(i--) == '1' ? '0' : '1';
-                sb.append(c);
-            }
-        }
-        return sb.charAt(k - 1);
-    }
-
-    public char findKthBit_dfs(int n, int k) {
         if (k == 1) {
             return '0';
         }
-        int mid = 1 << (n - 1);
+        int mid = (int) Math.pow(2, n - 1);
         if (k == mid) {
             return '1';
         } else if (k < mid) {
             return findKthBit(n - 1, k);
         } else {
-            return invert(findKthBit(n - 1, mid * 2 - k));
+            return invert(findKthBit(n - 1, (int) Math.pow(2, n) - k));
         }
     }
 
