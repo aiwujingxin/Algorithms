a65876e6e0504ab5a4952739cf148dc8ea1f0fe3
jingxin.wu <jingxin.wu@bytedance.com>
2025-04-13 19:56:34 +0800

    背包

--- java/src/knowledge/algorithms/sort/QuickSelect.java
+++ java/src/knowledge/algorithms/sort/QuickSelect.java
@@ -2,36 +2,34 @@ package knowledge.algorithms.sort;
 
 import java.util.Random;
 
+import leetcode.problems.*;
+
+
 /**
  * @author wujingxinit@outlook.com
  * @date 2024/1/9 21:08
  * @description 快速选择
- * @see leetcode.problems.LeetCode215
- * @see leetcode.problems.LeetCode324
- * @see leetcode.problems.LeetCode347
- * @see leetcode.problems.LeetCode414
- * @see leetcode.problems.LeetCode462
+ * @see LeetCode215
+ * @see LeetCode324
+ * @see LeetCode347
+ * @see LeetCode414
+ * @see LeetCode462
  */
 
 public class QuickSelect implements TopK {
 
     @Override
     public int findKthLargest(int[] nums, int k) {
-        return findKthLargest(nums, 0, nums.length - 1, k);
+        return quickSelect(nums, 0, nums.length - 1, k);
     }
 
-    public int findKthLargest(int[] nums, int lo, int hi, int k) {
-        if (lo > hi) {
-            return -1;
-        }
+    public int quickSelect(int[] nums, int lo, int hi, int k) {
+        if (lo > hi) return -1;
         int index = partition(nums, lo, hi);
-        if (index == k - 1) {
-            return nums[index];
-        }
-        if (index > k - 1) {
-            return findKthLargest(nums, lo, index - 1, k);
-        }
-        return findKthLargest(nums, index + 1, hi, k);
+        int rank = index + 1;
+        if (rank == k) return nums[index];
+        if (rank > k) return quickSelect(nums, lo, index - 1, k);
+        return quickSelect(nums, index + 1, hi, k);
     }
 
     private int partition(int[] nums, int i, int j) {
@@ -39,13 +37,9 @@ public class QuickSelect implements TopK {
         swap(nums, rand, i);
         int pi = nums[i];
         while (i < j) {
-            while (i < j && nums[j] <= pi) {
-                j--;
-            }
+            while (i < j && nums[j] <= pi) j--;
             nums[i] = nums[j];
-            while (i < j && nums[i] >= pi) {
-                i++;
-            }
+            while (i < j && nums[i] >= pi) i++;
             nums[j] = nums[i];
         }
         nums[i] = pi;
