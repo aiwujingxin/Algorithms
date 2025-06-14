7144bdf96d7a08d6a1ac1eb5c3d698fa6fd65dbb
jingxin.wu <jingxin.wu@bytedance.com>
2025-06-14 23:17:30 +0800

    20250614 hot100

--- java/src/leetcode/problems/LeetCode253.java
+++ java/src/leetcode/problems/LeetCode253.java
@@ -8,25 +8,18 @@ import java.util.PriorityQueue;
  * @author jingxinwu
  * @date 2023.09.19 17:04
  * @link <a href="https://www.cnblogs.com/lightwindy/p/8577794.html">...</a>
+ * @description 给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和结束的时间 intervals[i] = [starti, endi] ，返回 所需会议室的最小数量 。
+ * 示例 1：
+ * 输入：intervals = [[0,30],[5,10],[15,20]]
+ * 输出：2
+ * 示例 2：
+ * 输入：intervals = [[7,10],[2,4]]
+ * 输出：1
  */
+
 public class LeetCode253 {
 
-    /**
-     * 给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和结束的时间 intervals[i] = [starti, endi] ，返回 所需会议室的最小数量 。
-     * <p>
-     * 示例 1：
-     * <p>
-     * 输入：intervals = [[0,30],[5,10],[15,20]]
-     * 输出：2
-     * 示例 2：
-     * <p>
-     * 输入：intervals = [[7,10],[2,4]]
-     * 输出：1
-     **/
     public int minMeetingRooms(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return 0;
-        }
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         for (int[] interval : intervals) {
--- java/src/leetcode/problems/LeetCode312.java
+++ java/src/leetcode/problems/LeetCode312.java
@@ -6,26 +6,26 @@ import java.util.Arrays;
  * @author wujingxinit@outlook.com
  * @date 2023/11/8 22:47
  * @link <a href="https://cloud.tencent.com/developer/article/1880884">dp</a>
+ * @description dp[i][j]：戳破开区间 (i, j) 之间的气球，所能获得的最大硬币数。
  */
 public class LeetCode312 {
 
     public int maxCoins(int[] nums) {
-        int n = nums.length;
-        int[] p = new int[n + 2];
-        p[0] = p[n + 1] = 1;
-        for (int i = 0; i < n; i++) {
+        int n = nums.length + 2;
+        int[] p = new int[n];
+        p[0] = p[n - 1] = 1;
+        for (int i = 0; i < nums.length; i++) {
             p[i + 1] = nums[i];
         }
-        // dp[i][j]：戳破开区间 (i, j) 之间的气球，所能获得的最大硬币数。
-        int[][] dp = new int[n + 2][n + 2];
-        for (int i = n; i >= 0; i--) {
-            for (int j = i + 1; j < n + 2; j++) {
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
                 for (int k = i + 1; k < j; k++) {
                     dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + p[i] * p[k] * p[j]);
                 }
             }
         }
-        return dp[0][n + 1];
+        return dp[0][n - 1];
     }
 
     private Integer[][] memo;
